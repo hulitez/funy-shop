e64ca29567be2afc07c57290c2abeb323c8f1a7b
Zdzisław Jędrzejczak <zdzislaw.jedrzejczak@etravel.pl>
2020-10-25 22:43:47 +0100

    src/src/Controller/DefaultController.php -- product list on home page with paginator functionality

--- src/src/Controller/DefaultController.php
+++ src/src/Controller/DefaultController.php
@@ -2,25 +2,33 @@
 
 namespace App\Controller;
 
-use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="index", methods={"GET"})
+     * @param Request $request
+     * @param ProductRepository $productRepository
      * @return Response
      */
-    public function index(): Response
+    public function list(Request $request, ProductRepository $productRepository)
     {
-        $products = $this->getDoctrine()
-            ->getRepository(Product::class)
-            ->findAll();
+        $page = $request->query->getInt('page', 1);
+        $offset = $page > 0 ? ($page -1) * ProductRepository::PAGINATOR_PER_PAGE : 0;
+        $paginator = $productRepository->getPaginator($offset);
 
         return $this->render('product/index.html.twig', [
-            'products' => $products,
+            'products' => $paginator,
+            'previous' => $page > 1 ? $page -1 : null,
+            'next' => round(count($paginator) / ProductRepository::PAGINATOR_PER_PAGE) > $page
+                ? $page + 1
+                : null
+            ,
         ]);
     }
 }
